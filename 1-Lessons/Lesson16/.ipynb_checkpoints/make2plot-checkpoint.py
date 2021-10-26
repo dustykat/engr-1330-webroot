f3c963f134896a70814a14c03e9bfb4dc2ca373b
theodore cleveland <ted.cleveland@gmail.com>
2021-10-26 14:37:50 +0000

    update lab16

--- 1-Lessons/Lesson16/.ipynb_checkpoints/make2plot-checkpoint.py
+++ 1-Lessons/Lesson16/.ipynb_checkpoints/make2plot-checkpoint.py
@@ -2,7 +2,7 @@ def make2plot(listx1,listy1,listx2,listy2,strlablx,strlably,strtitle):
     import matplotlib.pyplot as plt
     plt.figure(figsize = (10,5)) # build a square drawing canvass from figure class
     plt.plot(listx1,listy1, c='red', marker='v',linewidth=0) # basic data plot
-    plt.plot(listx2,listy2, c='blue',linewidth=1) # basic model plot
+    plt.plot(listx2,listy2, c='green',linewidth=1) # basic model plot
     plt.xlabel(strlablx)
     plt.ylabel(strlably)
     plt.legend(['Observations','Model'])# modify for argument insertion
