fcec940016b5abe54c25d3c7787c72ff8dbfa927
theodore cleveland <ted.cleveland@gmail.com>
2022-03-07 03:39:22 +0000

    add jb l19+20

---
+++ engr1330jb/_build/jupyter_execute/lessons/lesson19/lesson19.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <div class="alert alert-block alert-info">
+#     <b><h1>ENGR 1330 Computational Thinking with Data Science </h1></b> 
+# </div> 
+# 
+# Copyright © 2021 Theodore G. Cleveland and Farhang Forghanparast
+# 
+# Last GitHub Commit Date: 5 Mar 2022
+#     
+# # 19: Simulation
+# - Simulating random values
+# - Draw with/without replacement
+# - Histograms to check behavior
+# - Concept of an interval
+
+# In[ ]:
+
+
+
+
+
+# ## References
+
+# In[ ]:
+
+
+
+
---
+++ engr1330jb/_build/jupyter_execute/lessons/lesson20/lesson20.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <div class="alert alert-block alert-info">
+#     <b><h1>ENGR 1330 Computational Thinking with Data Science </h1></b> 
+# </div> 
+# 
+# Copyright © 2021 Theodore G. Cleveland and Farhang Forghanparast
+# 
+# Last GitHub Commit Date: 5 Mar 2022
+#     
+# # 20: Interval Estimates by Simulation
+# - Bootstrapping
+# - Prediction intervals
+# - Parameter intervals
+
+# In[ ]:
+
+
+
+
+
+# ## References
+
+# In[ ]:
+
+
+
+
